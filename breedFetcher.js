817e4b697fca8fc10bda95246aaec7f38888d105
Daniyar Bazakov <bazakovd@gmail.com>
2024-06-16 10:08:47 -0400

    refactor breedFetcher, add mocha test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,18 @@
 const needle = require('needle');
-const bread = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${bread}`;
 
-needle.get(`${url}`, (error, response, body) => {
-  if (error) {
-    console.log('error:', error);
-  } else if (body.length === 0) {
-    console.log("bread does not exist");
-  } else {
-    console.log(body[0]['description']);
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  
+  needle.get(url, (error, response, body) => {
+    if (error) {
+      callback(error, null); // When there's an error, pass the error to the callback
+    } else if (body.length === 0) {
+      callback(null, "Breed does not exist"); // If no breed is found
+    } else {
+      const description = body[0]['description'];
+      callback(null, description); // If breed is found, pass the description
+    }
+  });
+};
 
+module.exports = { fetchBreedDescription };
